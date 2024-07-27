b72fcd9b8865bd9b7a74189b9f60a634cacd5dc7
dagimbe <135522349+dagimbe@users.noreply.github.com>
2024-07-27 20:50:44 +0100

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const {app}=require(".")
-const connectDB = require("./config/db")
-const port=4000
-app.listen(port,async()=>{
-    connectDB()
-    console.log('server is running on port ',port)
-}
-)
