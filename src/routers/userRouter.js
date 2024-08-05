652c1feeb4c9ce17e788273c5df9396337db0e34
dagimbe <135522349+dagimbe@users.noreply.github.com>
2024-08-05 04:59:38 -0700

    Delete src directory

--- src/routers/userRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controller/userController');
-const authenticate = require('../middleWare/authenticate'); // Ensure 'middlewares' is correctly named
-
-// Route to get user profile, protected by authentication middleware
-router.get('/profile', authenticate, userController.getUserProfileHandler);
-
-module.exports = router;
