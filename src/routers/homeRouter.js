41a4324edeea2afb6587922888b282db5ea945c0
dagimbe <135522349+dagimbe@users.noreply.github.com>
2024-08-05 05:24:55 -0700

    Delete src directory

--- src/routers/homeRouter.js
+++
@@ -1,7 +0,0 @@
-
-const { Router } = require("express");
-const router=Router();
-router.get("",async (req, res)=>{
-    res.status(200).send({message:"wellcome to Adama homes website"})
-})
-module.exports = router
--- src/routers/userRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controller/userController');
-const authenticate = require('../middleWare/authenticate');
-
-
-router.get('/profile',  authenticate(), userController.getUserProfileHandler);
-
-module.exports = router;
